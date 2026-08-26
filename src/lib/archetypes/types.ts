1ba7144789fc737d3cb1e08928c64486b044d495
jb_DEV <duriankab@gmail.com>
2026-08-26 13:58:00 +0700

    Add: archetypes/types + worlds/worldRegistry

---
+++ src/lib/archetypes/types.ts
@@ -0,0 +1,25 @@
+/**
+ * Archetype type definitions
+ */
+
+export type ArchetypeId =
+  // Base 12 archetypes
+  | 'hero'
+  | 'shadow'
+  | 'sage'
+  | 'innocent'
+  | 'explorer'
+  | 'creator'
+  | 'caregiver'
+  | 'lover'
+  | 'jester'
+  | 'everyman'
+  | 'magician'
+  | 'outlaw'
+  // Hybrid 6 archetypes
+  | 'strategic_warrior'
+  | 'benevolent_leader'
+  | 'visionary_artist'
+  | 'wandering_rebel'
+  | 'warm_flirt'
+  | 'relatable_neighbor';
